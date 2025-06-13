d353abb2ee1733e2bb58ee50ef4f108ade78d84c
hanboli <litterpigger@gmail.com>
2025-06-13 15:41:01 +0800

    feat:export

---
+++ cip/index.ts
@@ -0,0 +1,56 @@
+// Export all CIP modules
+export {
+  newPaperEvent,
+  newAnnotationEvent,
+  newReviewEvent,
+  newAiAnalysisEvent,
+  newDiscussionEvent,
+  newReadPaperEvent,
+  newCoCreatePaperEvent,
+  toNostrEvent as toNostrEventOpenResearch
+} from './cip05/openresearch.js'
+
+export {
+  newLikeEvent,
+  newCollectEvent,
+  newShareEvent,
+  newCommentEvent,
+  newTagEvent,
+  newFollowEvent,
+  newUnfollowEvent,
+  newQuestionEvent,
+  newRoomEvent,
+  newMessageInRoomEvent,
+  toNostrEvent as toNostrEventSocial
+} from './cip06/social.js'
+
+// Export governance module
+export {
+  newPostEvent,
+  newProposeEvent,
+  newVoteEvent,
+  newInviteEvent,
+  newMintEvent,
+  toNostrEvent as toNostrEventGovernance
+} from './cip01/governance.js'
+
+// Export common graph module
+export {
+  toNostrEvent as toNostrEventCommonGraph
+} from './cip02/common_graph.js'
+
+// Export model graph module
+export {
+  toNostrEvent as toNostrEventModelGraph
+} from './cip03/modelgraph.js'
+
+// Export community module
+export {
+  toNostrEvent as toNostrEventCommunity
+} from './cip07/community.js'
+
+// Export core modules
+export * from './auth.js'
+export * from './constants.js'
+export * from './keys.js'
+export * from './subspace.js' 
